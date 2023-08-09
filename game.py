86c272895d61f8c2692f843bd05ab9ac5991316b
Houjo <lolikion22829@gmail.com>
2023-08-09 16:21:33 +0300

    /

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 from random import randint as r
 import time
 
-
+###всё это для создания окна+дисплея
 pygame.init()
 display_w=1200
 display_h=750
@@ -11,15 +11,15 @@ pygame.display.set_caption('Game1')
 icon=pygame.image.load('ico1.png')
 pygame.display.set_icon(icon)
 display.fill((0,0,0))
-clock=pygame.time.Clock()
+clock=pygame.time.Clock()   #это просто для обновления дисплея и контроля фпс крч
 
 
+### вот это всё чтобы задавать размер поля но я его закоментил нахуй пока что нет нужды можеш расскоментить и посмотреть
 # input_state=False
 # rd=False
 # msg = ''
 # bad=False
 # sz=10
-#
 # while not rd:
 #
 #     for event in pygame.event.get():
@@ -56,32 +56,33 @@ clock=pygame.time.Clock()
 #n=int(input('n='))
 
 
-n=4
-cells= [[tuple(r(0,255) for j in range(3)) for i in range(n)] for y in range(n)]
-goal= [cells[r(0,n-1)][r(0,n-1)],cells[r(0,n-1)][r(0,n-1)],cells[r(0,n-1)][r(0,n-1)]]
-cnt=[0,0,0]
-while len(set(goal))!=3 or cells[0][n-1] in goal:
+###тут задаю основные переменные
+n=4   #размер поля
+cells= [[tuple(r(0,255) for j in range(3)) for i in range(n)] for y in range(n)] #массив в котором хранятся цвета для каждой из клеток
+goal= [cells[r(0,n-1)][r(0,n-1)],cells[r(0,n-1)][r(0,n-1)],cells[r(0,n-1)][r(0,n-1)]]  #тут придумываю 3 цвета которые нужно будет собрать
+cnt=[0,0,0] #счётчик собранных цветов
+while len(set(goal))!=3 or cells[0][n-1] in goal: #костыль чтобы было ВОЗМОЖНО найти нужнеы цвета
     goal = [cells[r(0, n - 1)][r(0, n - 1)], cells[r(0, n - 1)][r(0, n - 1)], cells[r(0, n - 1)][r(0, n - 1)]]
-con=750/n
-player_x=125+con/4
+con=750/n #размер стороны квадрата
+player_x=125+con/4 #координаты игрока в пикселях
 player_y=750-3*(con/4)
-x_crd=0
+x_crd=0 #координаты игрока в СО связанной с клетками типа 1-й ряд 3-й столбец
 y_crd=n-1
-tm1=None
-dif=None
-win_st = False
+tm1=None #
+dif=None #это для подсчёта времени стояния на текущей клетке
+win_st = False #это чтобы активировать функции окончания игры
 lose_st=False
-st=time.time()
+st=time.time() #время начала игры
 time_lim=20
 
 
-def print_txt(txt,x,y,size=30,clr=(255,255,255)):
+def print_txt(txt,x,y,size=30,clr=(255,255,255)):  #это чтобы выводить текст на дисплей
     font_type=pygame.font.Font('better-vcr_0.ttf',size)
     text=font_type.render(txt,True,clr)
     display.blit(text,(x,y))
 
 
-def move(key):
+def move(key): #эта ответственна за передвижки она меняет координаты
     global player_x,player_y,con,x_crd,y_crd
     if key==pygame.K_UP and player_y!=con/4:
         player_y-=con
@@ -97,7 +98,7 @@ def move(key):
         x_crd-=1
 
 
-def restart():
+def restart(): #при рестарте заново инициализирует все переменные (да так нужно)
     global cells,goal,cnt,player_y,player_x,x_crd,y_crd,tm1,dif,win_st,lose_st,st,n
     cells = [[tuple(r(0, 255) for j in range(3)) for i in range(n)] for y in range(n)]
     goal = [cells[r(0, n - 1)][r(0, n - 1)], cells[r(0, n - 1)][r(0, n - 1)], cells[r(0, n - 1)][r(0, n - 1)]]
@@ -115,54 +116,56 @@ def restart():
     st = time.time()
 
 
-def change_clrs():
+def change_clrs(): #меняет все цвета (см. строки 153-155)
     global cells,n
     for i in range(n):
         for j in range(n):
             cells[i][j]=tuple(r(0,255) for j in range(3))
 
 
-def game():
+def game(): #НОУ КОММЕНТО
     global lose_st,win_st,dif,tm1,goal,cnt,cells,display,clock,time_lim
-    while True:
-        if cnt==[2,2,2]:
+    while True: #бесконечный(нет) цикл обработки событий
+        if cnt==[2,2,2]: #проверяем а вдрг уже собрал всё
             win_st=True
-        if lose_st==True:
+        if lose_st==True: #проверяем а вдруг проиграл
             lose()
-        else:
-            if win_st==False:
-                current_clr=cells[x_crd][y_crd]
-                if current_clr in goal:
+        else: #если ты НЕ ПРОИГРАЛ
+            if win_st==False:#если ты НЕ ВЫИГРАЛ
+                current_clr=cells[x_crd][y_crd] #сука это блядь интуитивно понятно разрази тебя варда но я напишу что ЭТО ЦВЕТ КЛЕТКИ НА КОТОРОЙ СТОИТ ИГРОК
+                if current_clr in goal: #если ты стоишь на цвете что нужно собрать
                     if tm1 is None:
-                        tm1=time.time()
+                        tm1=time.time() #время входа на клетку
                     else:
-                        dif=time.time()-tm1
-                        if dif>=2:
+                        dif=time.time()-tm1 # разница между текущим временем и временем входа на клетку чтоб понять скок стоишь уже
+                        if dif>=2: #если стоишь достаточно то +респект
                             cnt[goal.index(current_clr)] += 1
                             tm1=None
                             dif=None
-                else:
+                else:#если встал на ненужный цвет все счётчики времени сбросить
                     tm1=None
                     dif=None
 
-                for event in pygame.event.get():
-                    if event.type==pygame.QUIT:
+                for event in pygame.event.get(): #перебор ВСЕХ событий в приложении
+                    if event.type==pygame.QUIT:#ноу коменто
                         pygame.quit()
                         quit()
                     # if event.type==pygame.KEYDOWN:
                     #     if event.key==pygame.K_SPACE:
                     #         change_clrs()
-                    if event.type==pygame.KEYDOWN and event.key in [pygame.K_UP,pygame.K_DOWN,pygame.K_RIGHT,pygame.K_LEFT]:
+                    if event.type==pygame.KEYDOWN and event.key in [pygame.K_UP,pygame.K_DOWN,pygame.K_RIGHT,pygame.K_LEFT]:#ноу коменто
                         move(event.key)
 
-                pygame.draw.rect(display,(200,100,100),(125,0,750,750))
-                display.fill((0, 0, 0))
+                display.fill((0, 0, 0))#покраска дисплея
+                pygame.draw.rect(display,(200,100,100),(125,0,750,750)) #отрисовка игрового поля покачто не роляет но потом на месте заливки будет рамка красивая
 
-                for i in range(n):
+
+                for i in range(n):#отрисовка квадратиков цветовых
                     for j in range(n):
                         pygame.draw.rect(display, cells[i][j],(125+i*con,0+j*con,con,con))
-                pygame.draw.rect(display,(0,0,0),(player_x,player_y,con/2,con/2))
+                pygame.draw.rect(display,(0,0,0),(player_x,player_y,con/2,con/2)) #отрисовка игрока
 
+                ### отрисовка всей панельки справа
                 pygame.draw.rect(display, goal[0], (900,250,40,40))
                 print_txt(str(cnt[0])+"/2",950,255)
                 pygame.draw.rect(display, goal[1], (900, 350, 40, 40))
@@ -172,21 +175,21 @@ def game():
                 print_txt('game time:', 900, 55)
                 total_tm=round(time_lim-(time.time()-st),4)
 
-                if total_tm<=0:
+                if total_tm<=0:#проверка не вышло ли время
                     lose_st=True
                 else:
                     print_txt(str(total_tm), 900, 95)
 
-                if dif!=None:
+                if dif!=None:#отрисовка времени на текущей клетке
                     print_txt(str(round(dif,4)), 950, 555)
                 #pygame.draw.rect(display, current_clr, (950, 550, 60, 60))
-            else:
+            else: #если выиграл)))
                 win()
-        pygame.display.update()
+        pygame.display.update()#крч просто смена кадров
         clock.tick(60)
 
 
-def lose():
+def lose(): #активируется при проигрыше
     global clock,display
     while True:
         for event in pygame.event.get():
@@ -204,7 +207,7 @@ def lose():
         clock.tick(60)
 
 
-def win():
+def win(): #активируется при выигрыше
     global clock, display
     while True:
         for event in pygame.event.get():
@@ -223,4 +226,4 @@ def win():
         clock.tick(60)
 
 
-game()
+game() #запуск игры
